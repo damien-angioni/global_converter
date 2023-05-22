c51f2a4e67da337672a5306d7a36d12174811901
damien-angioni <damien.angioni@laplateforme.io>
2023-05-22 11:23:39 +0200

    add quit option
    -created quit option in main loop

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        while(true){
+        boolean main_loop=true;
+        while(main_loop){
             System.out.println("entrez un chiffre selon le type de valeur en entrée: ");
             System.out.println("1 = texte: ");
             System.out.println("2 = hexadecimal: ");
             System.out.println("3 = decimal: ");
-            System.out.println("4 = octal: ");
             System.out.println("5 = binaire: ");
+            System.out.println("6 = quitter: ");
             String main_menu= sc.nextLine();
             if (main_menu.equals("1")){
                 System.out.println("choisis: texte: ");
@@ -24,6 +25,9 @@ public class Main {
             }
             else if (main_menu.equals("5")) {
                 System.out.println("choisis: binaire: ");
+            }
+            else if (main_menu.equals("6")) {
+                main_loop=false;
             }
             else{
                 System.out.println("option indisponible: ");
